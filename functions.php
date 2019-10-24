a60cac033ca22db8fa40761b199ee05ab415091a
Jesper <jesper.lundqvist@gmail.com>
2019-10-24 12:55:44 +0200

    Added code blocks

--- functions.php
+++ functions.php
@@ -2,6 +2,15 @@
 
 declare(strict_types=1);
 
+/**
+ * Links the post id and author id togheter and returns the author name.
+ *
+ * @param array $posts
+ * @param array $authors
+ *
+ * @return string
+ */
+
 function getAuthor(array $posts, array $authors): string
 {
     foreach ($authors as $author) {
@@ -11,8 +20,17 @@ function getAuthor(array $posts, array $authors): string
     }
 }
 
+/**
+ * Converets an array of strings to integers and sorts them in order from which date comes first.
+ *
+ * @param array $date1
+ * @param array $date2
+ *
+ * @return string
+ */
+
 function sortFunction(array $date1, array $date2) : int {
     return strtotime($date2["date"]) - strtotime($date1["date"]);
 }
 
-usort($posts, "sortFunction");
+
--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 require __DIR__ . '/data.php';
 require __DIR__ . '/functions.php';
-
+usort($posts, "sortFunction");
 
 
 ?>
