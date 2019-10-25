086da95e4b947f72d83c3b3861b6a8a6a00fe7c4
Jesper <jesper.lundqvist@gmail.com>
2019-10-25 16:49:01 +0200

    Made som more changes in the CSS

--- index.php
+++ index.php
@@ -4,11 +4,7 @@ require __DIR__ . '/data.php';
 require __DIR__ . '/functions.php';
 usort($posts, "sortFunction");
 
-
-
-
 ?>
-
 
 <!DOCTYPE html>
 <html lang="en">
@@ -18,6 +14,8 @@ usort($posts, "sortFunction");
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="css/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Roboto+Mono&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/4d50ed984c.js" crossorigin="anonymous"></script>
     <title>Plain News</title>
 </head>
 
@@ -25,27 +23,27 @@ usort($posts, "sortFunction");
 
     <div class="container">
 
-        <nav>Plain News</nav>
+        <nav class="nav-bar">Plain News <i class="far fa-newspaper"></i></nav>
 
         <?php foreach ($posts as $post) : ?>
 
-            <article>
-
-                <div class="text">
-
-                    <h2><?= $post['title'] ?></h2>
-                    <p><?= file_get_contents($post['content']) ?></p>
-                    <p><?= getAuthor($post, $authors) ?></p>
-                    <p><?= $post['date'] ?></p>
-                    <div class="likes">
-                        <p>Likes: <?= $post['likes'] ?></p>
-                        <img src="./img/like.svg" class="like-img" alt="like" loading="lazy">
-                    </div>
-                </div>
+            <article class="article">
                 <div class="article-img">
                     <img src="/img/<?= $post['image'] ?>" alt="article-image" loading="lazy">
                 </div>
-
+                <div class="article-text">
+                    <h2 class="article-heading"><?= $post['title'] ?></h2>
+                    <p><?= file_get_contents($post['content']) ?></p>
+                    <div class="article-info">
+                        <div class="written-by">
+                            <p><?= getAuthor($post, $authors) ?></p>
+                            <p><?= $post['date'] ?></p>
+                        </div>
+                        <div class="likes">
+                            <p><i class="fas fa-heart"></i> <?= $post['likes'] ?></p>
+                        </div>
+                    </div>
+                </div>
             </article>
 
         <?php endforeach; ?>
